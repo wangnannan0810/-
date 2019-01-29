2bcd13b4e343b82d55b78f392e15d8dc862af45b
wangnannan0810 <787244290@qq.com>
2019-01-29 19:49:18 +0800

    1.29

--- src/router/router.js
+++ src/router/router.js
@@ -4,9 +4,20 @@ const home = r => require.ensure([], () => r(require('../page/home/home')), 'hom
 const login = r => require.ensure([], () => r(require('../page/login/login')), 'login')
 const city = r => require.ensure([], () => r(require('../page/city/city')), 'city')
 const msite = r => require.ensure([], () => r(require('../page/msite/msite')), 'msite')
-
-
-
+const food = r => require.ensure([], () => r(require('../page/food/food')), 'food')
+const search = r => require.ensure([], () => r(require('../page/search/search')), 'search')
+const foodDetail = r => require.ensure([], () => r(require('../page/shop/children/foodDetail')), 'foodDetail')
+const shopDetail = r => require.ensure([], () => r(require('../page/shop/children/shopDetail')), 'shopDetail')
+const shopSafe = r => require.ensure([], () => r(require('../page/shop/children/children/shopSafe')), 'shopSafe')
+const shop = r => require.ensure([], () => r(require('../page/shop/shop')), 'shop')
+const confirmOrder = r => require.ensure([], () => r(require('../page/confirmOrder/confirmOrder')), 'confirmOrder')
+const remark = r => require.ensure([], () => r(require('../page/confirmOrder/children/remark')), 'remark')
+const invoice = r => require.ensure([], () => r(require('../page/confirmOrder/children/invoice')), 'invoice')
+const chooseAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/chooseAddress')), 'chooseAddress')
+const addAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/addAddress')), 'addAddress')
+const searchAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/children/searchAddress')), 'searchAddress')
+const payment = r => require.ensure([], () => r(require('../page/confirmOrder/children/payment')), 'payment')
+const userValidation = r => require.ensure([], () => r(require('../page/confirmOrder/children/userValidation')), 'userValidation')
 export default [{
     path: '/',
     component: App, //顶层路由，对应index.html
@@ -36,6 +47,61 @@ export default [{
         path: '/msite',
         component: msite,
         meta: { keepAlive: true },
+    },
+    //特色商铺列表页
+    {
+        path: '/food',
+        component: food
+    },
+      //搜索页
+      {
+        path: '/search/:geohash',
+        component: search
+    },
+      //商铺详情页
+      {
+        path: '/shop',
+        component: shop,
+        children: [{
+            path: 'foodDetail', //食品详情页
+            component: foodDetail,
+        }, {
+            path: 'shopDetail', //商铺详情页
+            component: shopDetail,
+            children: [{
+                path: 'shopSafe', //商铺安全认证页
+                component: shopSafe,
+            }, ]
+        }]
+    },
+     //确认订单页
+     {
+        path: '/confirmOrder',
+        component: confirmOrder,
+        children: [{
+            path: 'remark', //订单备注
+            component: remark,
+        }, {
+            path: 'invoice', //发票抬头
+            component: invoice,
+        }, {
+            path: 'payment', //付款页面
+            component: payment,
+        }, {
+            path: 'userValidation', //用户验证
+            component: userValidation,
+        }, {
+            path: 'chooseAddress', //选择地址
+            component: chooseAddress,
+            children: [{
+                path: 'addAddress', //添加地址
+                component: addAddress,
+                children: [{
+                    path: 'searchAddress', //搜索地址
+                    component: searchAddress,
+                }]
+            }, ]
+        }, ]
     },
     ]
 }]
